fb17631afebf1ad32e67361d12fb4a6feaba0e8b
LexuTros <lennart@toellke.com>
2021-12-10 12:21:28 +0100

    added game bat file

---
+++ HackerGame.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Lenny\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\Lenny\Documents\Schule\Studium\Informatik\Programmieren\Accees Woche 12\HackerGame\game.py"
+pause
